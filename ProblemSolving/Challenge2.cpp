064343a134a954f6d53e44cdb61f105abc9c340f
Hadil Yahi <165954236+hadilyahi@users.noreply.github.com>
2025-02-22 00:43:13 +0100

    solve the second  problem in challenge 2

--- ProblemSolving/Challenge2.cpp
+++ ProblemSolving/Challenge2.cpp
@@ -3,18 +3,32 @@
 using namespace std;
 
 // problem 1 :
-std::string even_or_odd(int number) 
+string even_or_odd(int number) 
 {
   return number % 2 == 0 ? "Even" : "Odd";
 }
-
+// problem 2 :
+string reverseString (string str){
+    string reversed = "" ;
+    for ( int i = str.length() - 1 ; i>=0 ; i-- ){
+         reversed += str[i];
+    }
+    return reversed;
+}
 int main ()
 {
-    // problem 1 :
-    int number ;
-   cout << "enter number : ";
-   cin >> number ;
-   cout << "The number is : " << even_or_odd (number) << endl;
-   
+    /////// problem 1 :
+    //   int number ;
+    //   cout << "enter number : ";
+    //   cin >> number ;
+    //   cout << "The number is : " << even_or_odd (number) << endl;
+    /////// Problem 2 : 
+    string str ;
+    cout << "enter string " ;
+    cin >> str ;
+    cout << " The string after Reversed : " << reverseString(str) << endl ;
+
+
+
 
 }
